c35a7335d04ace52f92f95322a6de9bdcc5e4126
Mike Seese <seesemichaelj@gmail.com>
2025-02-03 06:16:59 +0000

    fix issues with email rendering

--- docusaurus/gen-email.js
+++ docusaurus/gen-email.js
@@ -2,12 +2,46 @@
 const path = require("path");
 const fs = require("fs");
 const { JSDOM } = require("jsdom");
+const juice = require("juice");
 
 const customCss = `
   .main-wrapper {
     max-width: 37.5rem;
     margin: auto;
   }
+
+  .avatar__intro {
+    display: block !important;
+  }
+
+  main {
+    max-width: 100% !important;
+    flex: initial !important;
+  }
+`;
+
+const unsubscribeFooter = `
+<div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5">
+<div class="Unsubscribe--addressLine">
+  <p class="Unsubscribe--senderName"
+    style="font-size:12px;line-height:20px"
+  >
+    {{Sender_Name}}
+  </p>
+  <p style="font-size:12px;line-height:20px">
+    <span class="Unsubscribe--senderAddress">{{Sender_Address}}</span>, <span class="Unsubscribe--senderCity">{{Sender_City}}</span>, <span class="Unsubscribe--senderState">{{Sender_State}}</span> <span class="Unsubscribe--senderZip">{{Sender_Zip}}</span>
+  </p>
+</div>
+<p style="font-size:12px; line-height:20px;">
+  <a class="Unsubscribe--unsubscribeLink" href="{{{unsubscribe}}}" target="_blank" style="font-family:sans-serif;text-decoration:none;">
+    Unsubscribe
+  </a>
+  -
+  <a href="{{{unsubscribe_preferences}}}" target="_blank" class="Unsubscribe--unsubscribePreferences" style="font-family:sans-serif;text-decoration:none;">
+    Unsubscribe Preferences
+  </a>
+</p>
+</div>
 `;
 
 (async () => {
@@ -56,12 +90,24 @@ const customCss = `
 
   headerSpan.innerHTML = `${headerSpan.innerHTML} · <a href="https://redwoodmultiplayer.com/blog/${blogPostName}?ref=newsletter" target="_blank">View in browser</a>`;
 
+  wrapper.querySelector("div>.row>aside").remove();
+  wrapper.querySelector("div>.row>div").remove();
   wrapper.querySelector("article").querySelector("footer").remove();
   wrapper.querySelector("main").querySelector("nav").remove();
 
-  const htmlBody = `<style>${css}${customCss}</style>${wrapper.outerHTML
+  const htmlBody = `${wrapper.outerHTML
     .replace(/src="\//g, `src="https://redwoodmultiplayer.com/`)
-    .replace(/href="\//g, `href="https://redwoodmultiplayer.com/`)}`;
+    .replace(
+      /href="\//g,
+      `href="https://redwoodmultiplayer.com/`,
+    )}${unsubscribeFooter}`;
 
-  clipboardy.default.write(htmlBody);
+  const inlineHtml = juice(htmlBody, {
+    extraCss: css + customCss,
+    preserveFontFaces: false,
+    insertPreservedExtraCss: false,
+    preserveImportant: true,
+  });
+
+  clipboardy.default.write(inlineHtml);
 })();
